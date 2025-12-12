7f7810e11c867400f616df89b192a70f0d91beb4
OniwaPenut <137640432+PenutCreation@users.noreply.github.com>
2025-12-13 03:39:37 +0800

    Update and rename serverload6Gdata-impound8283AKKL01.js to KEY-AEUO29892HEU28839BAP922890SJEO2928429.js

--- KEY-AEUO29892HEU28839BAP922890SJEO2928429.js
+++ KEY-AEUO29892HEU28839BAP922890SJEO2928429.js
@@ -1,12 +1,11 @@
-
-  function loadDatabaseAndMain() {
+function loadDatabaseAndMain() {
     // Load DATABASEPOSTS.js with cache-busting query
     const dbScript = document.createElement("script");
-    dbScript.src = `https://cdn.jsdelivr.net/gh/PenutCreation/SERVER-MEKO-HYPERZ.G5z/DATABASEPOSTS.js?v=${Date.now()}`;
+    dbScript.src = `DATABASELOAD_API-28SKDJDOKEO2982KDJHVDUEK2982KD.js?v=${Date.now()}`;
     dbScript.onload = () => {
       // Load main.js after database is loaded
       const mainScript = document.createElement("script");
-      mainScript.src = "main.js";
+      mainScript.src =`DATABASELOAD_RULES_API-AE2828U292882AJDKOEID82SJJS28DKI38ASU.js?v=${Date.now()}`;
       document.body.appendChild(mainScript);
     };
     document.body.appendChild(dbScript);
