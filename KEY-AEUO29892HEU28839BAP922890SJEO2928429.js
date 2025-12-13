4a3e1a52a501c19f1cbc7664a5840480dd7b0546
OniwaPenut <137640432+PenutCreation@users.noreply.github.com>
2025-12-13 09:35:30 +0800

    Update database script source to CDN

--- KEY-AEUO29892HEU28839BAP922890SJEO2928429.js
+++ KEY-AEUO29892HEU28839BAP922890SJEO2928429.js
@@ -1,7 +1,7 @@
 function loadDatabaseAndMain() {
     // Load DATABASEPOSTS.js with cache-busting query
     const dbScript = document.createElement("script");
-    dbScript.src = `DATABASELOAD_API-28SKDJDOKEO2982KDJHVDUEK2982KD.js?v=${Date.now()}`;
+    dbScript.src = `https://cdn.jsdelivr.net/gh/PenutCreation/SERVER-MEKO-HYPERZ.G5z/DATABASELOAD_API-28SKDJDOKEO2982KDJHVDUEK2982KD.js?v=${Date.now()}`;
     dbScript.onload = () => {
       // Load main.js after database is loaded
       const mainScript = document.createElement("script");
