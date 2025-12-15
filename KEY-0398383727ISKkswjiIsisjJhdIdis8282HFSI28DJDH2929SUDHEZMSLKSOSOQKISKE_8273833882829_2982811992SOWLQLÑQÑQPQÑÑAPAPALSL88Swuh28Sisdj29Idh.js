6985f6797d3d1f317055c30ffac1142c550505d8
OniwaPenut <137640432+PenutCreation@users.noreply.github.com>
2025-12-15 15:55:18 +0800

    Rename JavaScript file for consistency

--- KEY-0398383727ISKkswjiIsisjJhdIdis8282HFSI28DJDH2929SUDHEZMSLKSOSOQKISKE_8273833882829_2982811992SOWLQLÑQÑQPQÑÑAPAPALSL88Swuh28Sisdj29Idh.js
+++ KEY-0398383727ISKkswjiIsisjJhdIdis8282HFSI28DJDH2929SUDHEZMSLKSOSOQKISKE_8273833882829_2982811992SOWLQLÑQÑQPQÑÑAPAPALSL88Swuh28Sisdj29Idh.js
@@ -1,7 +1,7 @@
 function loadDatabaseAndMain() {
     // Load DATABASEPOSTS.js with cache-busting query
     const dbScript = document.createElement("script");
-    dbScript.src = `https://cdn.jsdelivr.net/gh/PenutCreation/MEKO/ajaxent/DATABASELOAD_RULES_API-AE2828U292882AJDKOEID82SJJS28DKI38ASU.js`;
+    dbScript.src = `https://cdn.jsdelivr.net/gh/PenutCreation/MEKO/ajaxent/ajaxent/DATABASELOAD_RULES_API-AE2828U292_KAKASK_SODI_SKFN_OSID_ISKD882AJDKOEID82SJJS28DKI38ASU.js`;
     dbScript.onload = () => {
       // Load main.js after database is loaded
       
